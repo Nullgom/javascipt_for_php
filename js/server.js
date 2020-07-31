2292a4049b8d42e9e37a1bbdfd2ede036f6e164e
coddingbear <surfwon@naver.com>
2020-07-31 18:07:03 +0900

    JS Getting a Handle on this

--- js/server.js
+++ js/server.js
@@ -5,6 +5,7 @@ var app = express();
 var fs = require("fs");
 
 var makeGreeter = require("./lib/makeGreeter");
+var Tweet = require("./lib/tweet");
 var hello = makeGreeter("hello");
 var howdy = makeGreeter("howdy partner, how are you", "?");
 
@@ -20,4 +21,18 @@ app.get("/howdy/:from", function (req, res) {
     res.send(howdy(req.params.from.toUpperCase()));
 });
 
+app.get("/tweets/?(:id)?", function(req, res) {
+    var tweets = fs.readFileSync("../php/data/tweets.json").toString();
+    if(!req.params.id) {
+        res.send(tweets);
+        return;
+    }
+    var tweet = JSON.parse(tweets)[req.params.id];
+    tweet.id = req.params.id;
+    var getTweetJSON = Tweet.prototype.getJSON.bind(tweet);
+
+    // res.send(Tweet.prototype.getJSON.call(tweet));
+    res.send(getTweetJSON());
+
+});
 app.listen(3000);
