8186421a21b5a5d173d5eaea4d62142e1a5a5553
coddingbear <surfwon@naver.com>
2020-07-31 17:24:17 +0900

    Modules for Dpeenduce Management

--- js/server.js
+++ js/server.js
@@ -1,27 +1,23 @@
 'use strict';
-(function() {
-    // code...
-}());
 var express = require("express");
 var app = express();
 
 var fs = require("fs");
 
-
-var myvar = 100;
-
-function makeGreeter(greeting, punc) {
-    punc = punc || "!!";
-
-    return function(sender) {
-        return sender.trim() + " says " + greeting.trim() + punc.trim();
-    }
-}
+var makeGreeter = require("./lib/makeGreeter");
 var hello = makeGreeter("hello");
 var howdy = makeGreeter("howdy partner, how are you", "?");
+
 app.get("/", function (req, res) {
     res.send(howdy("JS", "🤷‍♀👍"));
+});
 
+app.get("/hello/:from", function (req, res) {
+    res.send(hello(req.params.from.toUpperCase()));
+});
+
+app.get("/howdy/:from", function (req, res) {
+    res.send(howdy(req.params.from.toUpperCase()));
 });
 
 app.listen(3000);
